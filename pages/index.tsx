e83df9b00fe7116e3750c4df4cd2fdc817e8852c
seongyunLim <lsy96999@gmail.com>
2021-06-03 02:05:14 +0900

    add tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,28 @@
 import React, {useState} from 'react'
 import Head from 'next/head'
 
-export default function Home(){
-  const [text, setText] = useState<string>("자바스크립트")
-  setTimeout(()=>{
-    setText("하하")
-  }, 1000)
+// export default function Home(){
+//   const [text, setText] = useState<string>("자바스크립트")
+//   setTimeout(()=>{
+//     setText("하하")
+//   }, 1000)
 
-  return (
-    <div className="container">
-      <div>
-        <span>{text} 적용완료</span>
-      </div>
-    </div>
-  )
+//   return (
+//     <div className="container">
+//       <div>
+//         <span>{text} 적용완료</span>
+//       </div>
+//     </div>
+//   )
+// }
+function clickHandler(){
+console.log('zz')
 }
+export default function Home() {
+  return (
+    <button className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
+    onClick={clickHandler}>
+      Click me
+    </button>
+  );
+ }
