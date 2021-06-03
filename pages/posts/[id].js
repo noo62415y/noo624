a1f9a705fcf14d9527c3a82c61b06d61858731c7
seongyunLim <lsy96999@gmail.com>
2021-06-04 00:51:47 +0900

    bb

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -8,6 +8,8 @@ export default function Post({postData}){
         {postData.id}
         <br />
         {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{__html:postData.contentHtml}}></div>
         </>
     )
 }
@@ -22,7 +24,7 @@ export async function getStaticPaths(){
 }
 export async function getStaticProps({params}){
     //fetching data using params.id
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
         props: {
             postData
